00a78c6695f889b6c18c533b778c4d6806af2865
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-12-07 12:44:41 +0200

    admin js function were add but not completed

--- script/admin.js
+++ script/admin.js
@@ -11,7 +11,7 @@ function adminContent(){
         adminTable.innerHTML = ''
         products.forEach( (product, i) =>{
             adminTable.innerHTML += `
-            <tr>
+            <tr class="text-center">
                 <td> ${product.name} </td>
                 <td> <img src="${product.image}" id="adminImg"></td>
                 <td> <button id="adminEdit"> <i class="bi bi-magic"></i> </button></td>
@@ -24,3 +24,36 @@ function adminContent(){
 }
 
 adminContent()
+
+let add = document.querySelector('[admin-add]')
+
+
+//not working but is the code from products js
+let sort = document.querySelector('[admin-sort]')
+// function adminSort() {
+//     product.sort((a, b) => {
+//         return a.name.localeCompare(b.name);
+//     });
+//     adminContent();
+// }
+// sort.addEventListener('click', adminSort)
+
+
+
+
+
+
+
+//for the delete function for admin edits 
+// function deleteProduct(){
+//     try{
+//         let index = products.findIndex(a =>{
+//             return a.id == item.id
+//         })
+//         products.splice(index, 1)
+//         localStorage.setItem('products', JSON.stringify(products))
+//     }catch(e){
+//         console.log(e.message);
+//     }
+// }
+
